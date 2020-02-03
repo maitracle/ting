d107b65d82def5c8c5b8d62a8ade570b6290233d
10000001a <pgdn06288888@gmail.com>
2020-02-03 16:09:36 +0900

    Make CoinHistory Model.

--- hongaeting/self_date/models.py
+++ hongaeting/self_date/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+from common.models import BaseModel
+
+
 # Create your models here.
+class CoinHistory(BaseModel):
+    CHANGE_REASON = (
+        ('Signup'),
+        ('Consume'),
+        ('Refund'),
+    )
+    user = models.ForeignKey('User', on_delete=models.DO_NOTHING)
+    rest_coin = models.IntegerField()
+    reason = models.CharField(max_length=50, choices=CHANGE_REASON)
