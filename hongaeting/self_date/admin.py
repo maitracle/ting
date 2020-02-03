e85a02539e81e0814e1ccb1e4ce82303b71d2506
outwater <kocon135@naver.com>
2020-02-03 18:16:26 +0900

    Like테이블 admin등록

--- hongaeting/self_date/admin.py
+++ hongaeting/self_date/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.admin import register
+
+from self_date.models import Like
+
+
+@register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    pass
+
 
 # Register your models here.
--- hongaeting/self_date/models.py
+++ hongaeting/self_date/models.py
@@ -4,7 +4,7 @@ from users.models import User
 from common.models import BaseModel
 
 class Like(BaseModel):
-    user = models.ForeignKey(User, on_delete=models.DO_NOTHING())
-    liked_user = models.ForeignKey(User, on_delete=models.DO_NOTHING())
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    liked_user = models.ForeignKey(User, related_name='liked_user', on_delete=models.DO_NOTHING)
 
 # Create your models here.
