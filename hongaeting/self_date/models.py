2aa85d2333b8adff07d9ddb004ba8de010108b7d
outwater <kocon135@naver.com>
2020-02-03 17:03:18 +0900

    Like모델생성

--- hongaeting/self_date/models.py
+++ hongaeting/self_date/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+from users.models import User
+from common.models import BaseModel
+
+class Like(BaseModel):
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING())
+    liked_user = models.ForeignKey(User, on_delete=models.DO_NOTHING())
+
 # Create your models here.
