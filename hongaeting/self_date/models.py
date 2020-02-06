cf72179c616c834da27f3c25ebd46976b9327de2
10000001a <47503412+10000001a@users.noreply.github.com>
2020-02-06 15:49:03 +0900

    Make CoinHistory Model. (#5)
    * Make CoinHistory Model.
    
    * Add CoinHistory to admin page.
    Make choices iterable.
    
    * Setting problem
    
    * add: list api 추가
    
    * ADD: CoinHistoryViewSet action 추가
    
    * DEL: self_date app에서 tests.py 파일 삭제
    
    * CHG: requirements.txt 파일 업데이트
    
    Co-authored-by: maitracle <39800081+maitracle@users.noreply.github.com>

--- hongaeting/self_date/models.py
+++ hongaeting/self_date/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from model_utils import Choices
+
+from common.models import BaseModel
+
 
 # Create your models here.
+class CoinHistory(BaseModel):
+    CHANGE_REASON = Choices('SIGNUP', 'CONSUME', 'REFUND', )
+
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    rest_coin = models.IntegerField()
+    reason = models.CharField(max_length=50, choices=CHANGE_REASON)
--- hongaeting/self_date/views.py
+++ hongaeting/self_date/views.py
@@ -1,3 +1,63 @@
-from django.shortcuts import render
+from django_rest_framework_mango.mixins import QuerysetMixin, SerializerMixin
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.mixins import ListModelMixin
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-# Create your views here.
+from self_date.models import CoinHistory
+from self_date.serializer import CreateCoinHistorySerializer, ListCoinHistorySerializer
+
+
+class CoinHistoryViewSet(
+    QuerysetMixin, SerializerMixin,
+    ListModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = CoinHistory.objects.all()
+    permission_classes = (IsAuthenticated,)
+    serializer_class = ListCoinHistorySerializer
+    serializer_class_by_actions = {
+        'list': ListCoinHistorySerializer,
+        'consume': CreateCoinHistorySerializer,
+        'refund': CreateCoinHistorySerializer,
+    }
+
+    @action(detail=False, methods=['post'])
+    def consume(self, request):
+        last_coin_history = self.get_queryset().last()
+        data = {
+            "user": request.user.id,
+            "rest_coin": last_coin_history.rest_coin - 1,
+            "reason": CoinHistory.CHANGE_REASON.CONSUME,
+        }
+
+        return self._create_coin_history(data)
+
+    @action(detail=False, methods=['post'])
+    def refund(self, request):
+        last_coin_history = self.get_queryset().last()
+        data = {
+            "user": request.user.id,
+            "rest_coin": last_coin_history.rest_coin + 1,
+            "reason": CoinHistory.CHANGE_REASON.REFUND,
+        }
+
+        return self._create_coin_history(data)
+
+    def filtered_queryset_by_user(self, queryset):
+        return queryset.filter(user=self.request.user)
+
+    def list_queryset(self, queryset):
+        return self.filtered_queryset_by_user(queryset)
+
+    def consume_queryset(self, queryset):
+        return self.filtered_queryset_by_user(queryset)
+
+    def _create_coin_history(self, data):
+        serializer = self.get_serializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
