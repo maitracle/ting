8f556e97306f3270360ccafc57895b7750881c03
maitracle <maitracle@gmail.com>
2020-02-10 18:10:44 +0900

    ADD: local setting에 cors whitelist 추가

--- hongaeting/hongaeting/settings/base.py
+++ hongaeting/hongaeting/settings/base.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = DJANGO_APPS + \
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
--- hongaeting/hongaeting/settings/local.py
+++ hongaeting/hongaeting/settings/local.py
@@ -4,3 +4,9 @@ from hongaeting.settings.base import *
 PROFILE = 'Local'
 
 DEBUG = True
+CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ORIGIN_WHITELIST = (
+    'localhost:3000',
+    '127.0.0.1:3000'
+)
