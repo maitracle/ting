6b928723f1e987ed7f02e16892e0c72a559c1935
maitracle <maitracle@gmail.com>
2020-02-14 14:13:46 +0900

    ADD: camel case 관련 json parser 추가

--- hongaeting/hongaeting/settings/base.py
+++ hongaeting/hongaeting/settings/base.py
@@ -137,6 +137,15 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'djangorestframework_camel_case.render.CamelCaseJSONRenderer',
+        'djangorestframework_camel_case.render.CamelCaseBrowsableAPIRenderer',
+    ),
+    'DEFAULT_PARSER_CLASSES': (
+        'djangorestframework_camel_case.parser.CamelCaseFormParser',
+        'djangorestframework_camel_case.parser.CamelCaseMultiPartParser',
+        'djangorestframework_camel_case.parser.CamelCaseJSONParser',
+    ),
 }
 
 # simple jwt
