97def207f2c101d536242c4cc12af3e487563029
maitracle <39800081+maitracle@users.noreply.github.com>
2020-02-14 14:40:45 +0900

    CHG: cors whitelist에 protocole 추가 (#16)
    * CHG: cors whitelist에 protocole 추가
    
    * ADD: camel case 관련 json parser 추가

--- hongaeting/hongaeting/settings/local.py
+++ hongaeting/hongaeting/settings/local.py
@@ -8,8 +8,10 @@ DEBUG = True
 CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ORIGIN_WHITELIST = (
-    'localhost:3000',
-    '127.0.0.1:3000'
+    'https://localhost:3000',
+    'https://127.0.0.1:3000',
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
 )
 
 
