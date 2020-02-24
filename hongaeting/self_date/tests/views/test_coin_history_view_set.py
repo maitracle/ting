252407d8a7fead9b86d68368fd5687968ad28777
maitracle <39800081+maitracle@users.noreply.github.com>
2020-02-24 15:20:03 +0900

    Feat/check view profile (#22)
    * CHG: coin history model에 profile field추가, consume, refund api 제거
    
    * CHG: profile list api에 is_viewed field 추가

--- hongaeting/self_date/tests/views/test_coin_history_view_set.py
+++ hongaeting/self_date/tests/views/test_coin_history_view_set.py
@@ -3,15 +3,13 @@ from model_bakery import baker
 from rest_framework import status
 from rest_framework.test import APITestCase
 
-from self_date.models import CoinHistory
-
 
 class CoinHistoryTestCase(APITestCase):
     def test_should_get_list(self):
         # Given: user 1명이 생성되고 그 유저의 coin-history가 생성된다.
         user = baker.make('users.User')
         coin_history_quantity = 3
-        baker.make('self_date.CoinHistory', user=user, _quantity=coin_history_quantity)
+        expected_coin_histories = baker.make('self_date.CoinHistory', user=user, _quantity=coin_history_quantity)
 
         # When: user가 coin_history_list api 호출
         self.client.force_authenticate(user=user)
@@ -20,38 +18,20 @@ class CoinHistoryTestCase(APITestCase):
         # Then: response값이 정상적으로 온다.
         assert_that(response.status_code).is_equal_to(status.HTTP_200_OK)
         assert_that(response.data).is_length(coin_history_quantity)
-        for i in range(coin_history_quantity):
-            assert_that(response.data[i]['user']).is_equal_to(user.id)
 
+        for responded_coin_history, expected_coin_history in zip(response.data, expected_coin_histories):
+            assert_that(responded_coin_history['user']).is_equal_to(user.id)
+            assert_that(responded_coin_history['rest_coin']).is_equal_to(expected_coin_history.rest_coin)
+            assert_that(responded_coin_history['reason']).is_equal_to(expected_coin_history.reason)
+            assert_that(responded_coin_history['profile']).is_equal_to(expected_coin_history.profile)
 
-    def test_should_consume(self):
-        # Given: user 1명과 그의 Signup coin_history가 생성된다.
-        user = baker.make('users.User')
-        user_rest_coin = 2
-        baker.make('self_date.CoinHistory', user=user, reason=CoinHistory.CHANGE_REASON.SIGNUP,
-                   rest_coin=user_rest_coin)
+            assert_that(responded_coin_history['created_at'])\
+                .is_equal_to(self._reformat_datetime(expected_coin_history.created_at))
+            assert_that(responded_coin_history['updated_at'])\
+                .is_equal_to(self._reformat_datetime(expected_coin_history.updated_at))
 
-        # When: user가 consume api를 호출한다.
-        self.client.force_authenticate(user=user)
-        response = self.client.post('/api/coin-histories/consume/')
+    @staticmethod
+    def _reformat_datetime(datetime):
+        date, time = str(datetime).split(' ')
 
-        # Then: response값이 정상적으로 온다.
-        assert_that(response.status_code).is_equal_to(status.HTTP_201_CREATED)
-        assert_that(response.data['rest_coin']).is_equal_to(user_rest_coin - 1)
-        assert_that(CoinHistory.objects.last().rest_coin).is_equal_to(user_rest_coin - 1)
-
-    def test_should_refund_coin_history(self):
-        # Given: user 1명과 그의 Signup coin_history가 생성된다.
-        user = baker.make('users.User')
-        user_rest_coin = 2
-        baker.make('self_date.CoinHistory', user=user, reason=CoinHistory.CHANGE_REASON.SIGNUP,
-                   rest_coin=user_rest_coin)
-
-        # When: user가 refund api를 호출한다.
-        self.client.force_authenticate(user=user)
-        response = self.client.post('/api/coin-histories/refund/')
-
-        # Then: response값이 정상적으로 온다.
-        assert_that(response.status_code).is_equal_to(status.HTTP_201_CREATED)
-        assert_that(response.data['rest_coin']).is_equal_to(user_rest_coin + 1)
-        assert_that(CoinHistory.objects.last().rest_coin).is_equal_to(user_rest_coin + 1)
+        return f'{date}T{time.split("+")[0]}Z'
