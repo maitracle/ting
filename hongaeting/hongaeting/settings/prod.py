2b8b55c3bb22fb946143c33ebb52be841e605d93
maitracle <maitracle@gmail.com>
2020-03-09 17:54:19 +0900

    add: static root 추가

--- hongaeting/hongaeting/settings/prod.py
+++ hongaeting/hongaeting/settings/prod.py
@@ -6,3 +6,17 @@ PROFILE = 'Production'
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
+
+
+# Build paths
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+ROOT_DIR = os.path.dirname(BASE_DIR)
+
+# static files
+STATIC_URL = '/static/'
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
+
+STATIC_ROOT = os.path.join(ROOT_DIR, '.static_root')
