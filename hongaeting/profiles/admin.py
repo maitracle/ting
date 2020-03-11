e6cf4d81550ec836509d2bd79909ba08fbef3978
maitracle <39800081+maitracle@users.noreply.github.com>
2020-03-11 13:03:07 +0900

    QuestionList, QuestionItem model을 삭제합니다 (#43)
    * DEL: QuestionList, QuestionItem model 삭제
    
    * DEL: QuestionList, QuestionItem testcode 삭제

--- hongaeting/profiles/admin.py
+++ hongaeting/profiles/admin.py
@@ -1,18 +1,7 @@
 from django.contrib import admin
 from django.contrib.admin import register
 
-from profiles.models import QuestionList, QuestionItem, Profile
-
-
-@register(QuestionList)
-class QuestionListAdmin(admin.ModelAdmin):
-    list_display = ('name', 'university', 'season', 'version')
-    list_filter = ('university',)
-
-
-@register(QuestionItem)
-class QuestionItemAdmin(admin.ModelAdmin):
-    list_display = ('name',)
+from profiles.models import Profile
 
 
 @register(Profile)
