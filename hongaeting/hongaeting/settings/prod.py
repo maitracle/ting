293d593f4991e807c3b4992eb36ae74a780803b8
maitracle <39800081+maitracle@users.noreply.github.com>
2020-03-11 13:08:32 +0900

    Feat/prod setting (#42)
    * add: prod setting에 allowed host 추가
    
    * add: static root 추가
    
    * FIX: allowed host 수정
    
    * FIX: allowed host 수정
    
    * FIX: prod에서 allowed host 모두 허용
    
    * ADD: cors setting 추가

--- hongaeting/hongaeting/settings/prod.py
+++ hongaeting/hongaeting/settings/prod.py
@@ -4,3 +4,22 @@ from hongaeting.settings.base import *
 PROFILE = 'Production'
 
 DEBUG = False
+
+ALLOWED_HOSTS = ['*']
+
+# CORS
+CORS_ORIGIN_ALLOW_ALL = True
+
+
+# Build paths
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+ROOT_DIR = os.path.dirname(BASE_DIR)
+
+# static files
+STATIC_URL = '/static/'
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
+
+STATIC_ROOT = os.path.join(ROOT_DIR, '.static_root')
