cc0a6129a99adfccfb913c48ffa16f29862cbfcc
maitracle <39800081+maitracle@users.noreply.github.com>
2020-03-12 15:11:43 +0900

    Feat/nginx wsgi setting (#44)
    * CHG: wsgi 설정 수정, static root 수정
    
    * CHG: git ignore 경로 정리

--- hongaeting/hongaeting/wsgi.py
+++ hongaeting/hongaeting/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hongaeting.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hongaeting.settings.prod')
 
 application = get_wsgi_application()
